f153fdc53576e710edb8a12d76473c16f99d2565
ShiverAwe <quameu@gmail.com>
2018-07-03 23:40:42 +0300

    Basic project reformation.

---
+++ src/main/java/com/github/shevelevaanna/Enge.java
@@ -0,0 +1,7 @@
+package com.github.shevelevaanna;
+
+public class Enge {
+    public int eFirst;
+    public int eSecond;
+    public int w;
+}
---
+++ src/main/java/com/github/shevelevaanna/Main.java
@@ -0,0 +1,8 @@
+package com.github.shevelevaanna;
+
+public class Main {
+    public static void main(String[] args) {
+        MyGUIForm app = new MyGUIForm();
+        app.setVisible(true);
+    }
+}
